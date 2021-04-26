97b049219e18156b768bd968fd5b115939d7b9cf
MarcosAur <marcosaureliomendesoliveira@gmail.com>
2021-04-26 15:08:38 -0300

    Adicionando classe Seller e testando no programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -8,10 +12,14 @@ import model.entities.Department;
  */
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Locale.setDefault(Locale.US);
         
-        Department d = new Department(1, "T.I");
-        System.out.println(d);
+        Department d = new Department(1,"Desenvolvimento");
+        Seller seller = new Seller(1,"Marcos", "marcos@gmail.com", sdf.parse("02/04/2003"), 6000.00,d);
+        
+        System.out.println(seller);
     }
     
 }
