fd4fee45b4157f0c3b061088ce538b26eb23d770
MarcosAur <marcosaureliomendesoliveira@gmail.com>
2021-04-26 14:54:57 -0300

    Criação da classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,7 @@
 package application;
+
+import model.entities.Department;
+
 /**
  *
  * @author marcos
@@ -7,6 +10,8 @@ public class Program {
 
     public static void main(String[] args) {
         
+        Department d = new Department(1, "T.I");
+        System.out.println(d);
     }
     
 }
